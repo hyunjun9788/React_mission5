3c11d684546c5e81b64852e06dea8d9a8a7f0977
hyunjun9788 <namhyunjun9788@gmail.com>
2023-06-13 18:38:28 +0900

    feat: Layout폴더에 Header 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Layout/Header"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+
+            <section className="about">
+                <h2 className="about__title">About Me</h2>
+                <p className="about__content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc auctor augue
+                    ac mauris commodo, vel dapibus lorem eleifend.</p>
+            </section>
+
+            <section className="skills">
+                <h2 className="skills__title">My Skills</h2>
+                <ul className="skills__list">
+                    <li className="skills__item">HTML</li>
+                    <li className="skills__item">CSS</li>
+                    <li className="skills__item">JavaScript</li>
+                </ul>
+            </section>
+
+            <footer className="footer">
+                <p className="footer__text">&copy; 2023 My Website. All rights reserved.</p>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
